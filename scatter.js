a2afcd39e693353880d0b507aa13a57756dc5a2d
naotakak <naotakakinoshita@gmail.com>
2018-03-21 12:18:45 -0400

    base

---
+++ scatter.js
@@ -0,0 +1,25 @@
+/***
+yarp
+Naotaka Kinoshita & Vivien Lee
+SoftDev2 Pd7
+K13 -- Scattered
+2018-3-22
+***/
+
+var pic = document.getElementById("vimage");
+
+//data
+var x = [];
+var y = [];
+
+//plot function to take in data and graph
+var plot = function() {
+
+};
+
+//draw individual points on svg
+var drawPoint = function() {
+
+};
+
+plot();
